d04c7618d40fe08bd25e0a505130a9247370b453
KarlavdMerwe <70195420+KarlavdMerwe@users.noreply.github.com>
2021-09-28 12:17:36 +0200

    28 Sep 2021 Create AccountTypeController and GeneralResponse General Response debugging

--- AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTypeController.java
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTypeController.java
@@ -2,11 +2,14 @@ package za.ac.nwu.ac.web.sb.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-//import za.ac.nwu.ac.domain.service.GeneralResponse;
+import za.ac.nwu.ac.domain.service.GeneralResponse;
+
 
 @RestController
 public class AccountTypeController {
 
-    //@GetMapping("/all")
-    //public GeneralResponse<String> getAll(){return new GeneralResponse<String>( successfull: true, payload)}
+    @GetMapping("/all")
+    public GeneralResponse<String> getAll() {
+        return new GeneralResponse<String>(true, "Np types found");
+    }
 }
