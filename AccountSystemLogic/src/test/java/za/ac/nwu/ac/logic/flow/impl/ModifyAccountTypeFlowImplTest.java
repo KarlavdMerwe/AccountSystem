9b3e41f13617402fbca53a3d4513a16a3bd2391e
KarlavdMerwe <70195420+KarlavdMerwe@users.noreply.github.com>
2021-10-07 17:06:22 +0200

    7 Oct 2021 Code coverage and Unit Testing

---
+++ AccountSystemLogic/src/test/java/za/ac/nwu/ac/logic/flow/impl/ModifyAccountTypeFlowImplTest.java
@@ -0,0 +1,48 @@
+package za.ac.nwu.ac.logic.flow.impl;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import za.ac.nwu.ac.domain.persistence.AccountType;
+import za.ac.nwu.ac.repo.persistence.AccountTypeRepository;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class ModifyAccountTypeFlowImplTest {
+
+    private ModifyAccountTypeFlowImpl modifyAccountTypeFlow;
+
+    @Before
+    public void setUp() throws Exception {
+        modifyAccountTypeFlow = new ModifyAccountTypeFlowImpl();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Mock
+    AccountTypeRepository accountTypeRepository;
+
+    @Test
+    public void deleteAccountTypeTest() {
+//        AccountType accountType = new AccountType("MILES", "Miles", LocalDate.now());
+//        AccountType accountType2 = new AccountType("PLAY", "Play", LocalDate.now());
+//
+//        when(accountTypeRepository.getAccountTypeByMnemonic("MILES")).thenReturn(accountType2);
+//
+//        modifyAccountTypeFlow.deleteAccountType(String.valueOf(accountType));
+//
+//        verify(accountTypeRepository, times(1)).delete(accountType2);
+
+    }
+
+    @Test
+    public void updateAccountTypeTest() {
+    }
+}
